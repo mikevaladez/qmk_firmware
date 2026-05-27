cc7789640563d1e3c3a20364da676fba0c461631
mike <mike.valadez@gmail.com>
2026-05-27 12:26:00 -0500

    Swapped esc and grv

--- keyboards/keebio/iris/keymaps/mv-iris-v7/keymap.c
+++ keyboards/keebio/iris/keymaps/mv-iris-v7/keymap.c
@@ -7,7 +7,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
 	//--------------------------------------------------------------/              		             \--------------------------------------------------------------------
-	   KC_ESC,	  KC_1,   KC_2,   KC_3, 		 		KC_4,   KC_5, 						          KC_6,    KC_7,   KC_8,                 KC_9,     KC_0,      KC_MINS, 
+	   KC_GRV,	  KC_1,   KC_2,   KC_3, 		 		KC_4,   KC_5, 						          KC_6,    KC_7,   KC_8,                 KC_9,     KC_0,      KC_MINS, 
 	//--------------------------------------------------------------/            			         \--------------------------------------------------------------------
 	   KC_TAB,    KC_Q,   KC_W,   KC_E,        	        KC_R,   KC_T, 						          KC_Y,    KC_U,   KC_I,                 KC_O,     KC_P,      KC_BSLS, 
 	//--------------------------------------------------------------/              		   	         \--------------------------------------------------------------------
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//----------------------------------------------------------------------/  			 \--------------------------------------------------------------------------------
 	   KC_LCTL,   KC_Z,   KC_X,   KC_C, 				KC_V,   KC_B,   MO(1), 				 MO(1),   KC_N,    KC_M,   KC_COMM,              KC_DOT,   KC_SLSH,   KC_EQL,
 	//----------------------------------------------------------------------/            \-------------------------------------------------------------------------------
-											     KC_LALT, KC_LSFT, KC_SPC,   	         KC_ENT,  KC_GRV, TO(1)						
+											     KC_LALT, KC_LSFT, KC_SPC,   	         KC_ENT,  KC_ESC, TO(1)						
 										      //----------------------------/            \-----------------------
 	),
 	[1] = LAYOUT(	
@@ -68,7 +68,7 @@ static HSV layer_to_hsv(uint8_t layer) {
         case 0: return (HSV){HSV_BLUE};
         case 1: return (HSV){HSV_GREEN};
         case 2: return (HSV){HSV_PURPLE};
-        case 3: return (HSV){HSV_AZURE};
+        case 3: return (HSV){HSV_YELLOW};
         default: return (HSV){HSV_BLUE};
     }
 }
